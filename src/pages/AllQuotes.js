a120e8c85006e3186646ea585665ef97aaaafdeb
Hari Aryal <aryalhari059@gmail.com>
2025-02-09 14:32:24 +0545

    comment-section rem

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,38 @@
- const AllQuotes=()=>{
-    return<h1>All Quotes Page</h1>
- };
+import { useEffect } from 'react';
 
- export default AllQuotes;
+import QuoteList from '../components/quotes/QuoteList';
+import LoadingSpinner from '../components/ui/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
+
+const AllQuotes = () => {
+  const { sendRequest, status, data: loadedQuotes, error } = useHttp(
+    getAllQuotes,
+    true
+  );
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === 'pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className='centered focused'>{error}</p>;
+  }
+
+  if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;
+};
+
+export default AllQuotes;
